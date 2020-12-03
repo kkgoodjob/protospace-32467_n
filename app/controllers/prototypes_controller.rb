0bc5d76aa21301a56afe500ce3f384afd156da3e
kkgoodjob <70433180+kkgoodjob@users.noreply.github.com>
2020-12-03 15:26:31 +0900

    投稿機能を実装しましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,8 +3,20 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    Prototype.create(prototype_params)
+    if Prototype.create(prototype_params)
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
